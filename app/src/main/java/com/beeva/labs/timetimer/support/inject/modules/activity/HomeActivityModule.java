f483c28346d698e341b20dc631fbef4b9b9d31aa
alvaro.delaserna <alvaro.delaserna@bbva.com>
2017-03-10 09:32:48 +0100

    delete unnecessary things

--- app/src/main/java/com/beeva/labs/timetimer/support/inject/modules/activity/HomeActivityModule.java
+++ app/src/main/java/com/beeva/labs/timetimer/support/inject/modules/activity/HomeActivityModule.java
@@ -1,11 +1,8 @@
 package com.beeva.labs.timetimer.support.inject.modules.activity;
 
 import com.beeva.labs.timetimer.support.base.BaseActivity;
-import com.beeva.labs.timetimer.support.delegates.SharingUsers;
-import com.beeva.labs.timetimer.support.delegates.SharingUsersDelegate;
 import com.wokdsem.kinject.annotations.Includes;
 import com.wokdsem.kinject.annotations.Module;
-import com.wokdsem.kinject.annotations.Provides;
 
 @Module(completed = true)
 public class HomeActivityModule {
@@ -19,10 +16,5 @@ public class HomeActivityModule {
 	@Includes
 	public BaseActivityModule includeBaseActivityModule() {
 		return new BaseActivityModule(baseActivity);
-	}
-
-	@Provides(singleton = true)
-	public SharingUsers provideShareUsers() {
-		return new SharingUsersDelegate();
 	}
 }
