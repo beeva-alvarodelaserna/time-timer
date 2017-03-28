169a87c62ef2c8e833de4afb3971088c4e136809
alvaro.delaserna <alvaro.delaserna@bbva.com>
2017-03-28 15:36:59 +0200

    add ngCordova; add alarm sound

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,7 @@
 angular.module('starter.controllers', [])
-
     .controller('AppCtrl', function ($scope) {
 
     })
-
     .controller('HomeCtrl', function ($scope, $state) {
         $scope.goToSelectSession = function () {
             $state.go('app.selectSession');
--- www/js/finishedController.js
+++ www/js/finishedController.js
@@ -1,7 +1,21 @@
-angular.module('final.controllers')
-    .controller('FinishedCtrl', function ($scope, $state) {
+angular.module('finished.controllers', ['ngCordova'])
+    .controller('FinishedCtrl', function ($scope, $state, $ionicPlatform, $timeout, $cordovaNativeAudio) {
 
+        $scope.loaded = false;
+        // $ionicPlatform.ready(function () {
+        //     $cordovaNativeAudio.preloadSimple('alarm', '../audio/alarm.mp3');
+        // });
+        //
         $scope.goToSurvey = function () {
+            $scope.loaded = false;
             $state.go('app.survey');
+        };
+        //
+        // $scope.play = function (sound) {
+        //     $cordovaNativeAudio.play(sound);
+        // };
+
+        $scope.playAlarm = function () {
+            $scope.loaded = true;
         };
     });
